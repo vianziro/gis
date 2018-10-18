a01d0369ec60faba3484e95211007f6419a6c860
sayfudin binadarma <sayfudin1309@gmail.com>
2018-10-18 07:58:40 +0700

    Add files via upload

---
+++ home.php
@@ -0,0 +1,13 @@
+<div class="page-header">
+<header>
+<p></p>
+</header>
+    <h2>Website GIS Pembagian Lahan Pertanian dan Perkebunan di Kabupaten OKU TIMUR</h2>
+</div>
+<p><marquee>.Selamat Datang di website Sistem informasi geografis lahan pertanian Dinas Pertanian Ogan Komering Ulu Timur </marquee></p>
+<table width="200" border="0">
+  <tr>
+    <td><img src="gambar/lahanOKUT.jpg" width="1120" height="794" /></td>
+  </tr>
+</table>
+<p>&nbsp;</p>
